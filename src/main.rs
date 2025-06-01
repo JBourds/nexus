2cc436dcc7f68067ca967521359cc960357d7d3f
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-06-01 14:50:15 -0400

    cli: Setup clap to parse CLI arg for toml file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    /// Configuration toml file for the simulation
+    config: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let config_file = std::fs::read_to_string(args.config.as_str())
+        .context(format!("Unable to open file located at {}", args.config))?;
+    Ok(())
 }
