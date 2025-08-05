e49a387c1532fb3217c017cb4e158a3048653b9d
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-08-05 15:45:44 -0400

    config + runner: Putting AST types into their own module within config. New lib for running programs.

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -3,12 +3,14 @@ use anyhow::{Context, Result};
 pub(crate) mod parse;
 mod validate;
 
-pub use validate::*;
+pub mod ast {
+    pub use crate::validate::*;
+}
 
-pub fn parse(text: String) -> Result<Simulation> {
+pub fn parse(text: String) -> Result<ast::Simulation> {
     let parsed: parse::Simulation = toml::from_str(text.as_str())
         .context("Failed to parse simulation parameters from config file.")?;
-    let validated = Simulation::validate(parsed)
+    let validated = ast::Simulation::validate(parsed)
         .context("Failed to validate simulation parameters from config file.")?;
     println!("{validated:#?}");
     Ok(validated)
