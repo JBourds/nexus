2991fdfd935c9dd2542c79c62cbd39bd1e4c61a5
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-08-05 16:59:56 -0400

    runner: Basic ability to execute programs listed in config files. Soon to be client/server example that just prints for the moment.

---
+++ runner/src/errors.rs
@@ -0,0 +1,14 @@
+use std::{fmt::Display, os};
+
+use config::ast;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ProtocolError {
+    #[error("Node {node_name}: Error for protocol {protocol_name}: {msg}")]
+    RunnerError {
+        node_name: String,
+        protocol_name: String,
+        msg: String,
+    },
+}
--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -1,1 +1,43 @@
 use config::ast;
+use std::process::{Command, Output};
+use std::rc::Rc;
+use std::thread::{self, JoinHandle};
+pub mod errors;
+use errors::*;
+
+pub fn run(sim: ast::Simulation) -> Result<(), ProtocolError> {
+    let mut handles = vec![];
+    for (node_name, node) in sim.nodes {
+        for (protocol_name, protocol) in node.protocols {
+            handles.push((node_name.clone(), protocol_name, run_protocol(protocol)));
+        }
+    }
+
+    for (node_name, protocol_name, handle) in handles {
+        let res = handle.join().map_err(|e| ProtocolError::RunnerError {
+            node_name: node_name.clone(),
+            protocol_name: protocol_name.clone(),
+            msg: format!("{e:?}"),
+        })?;
+        println!("{node_name}: {protocol_name} - {res:#?}");
+    }
+
+    Ok(())
+}
+
+fn run_protocol(protocol: ast::NodeProtocol) -> JoinHandle<Output> {
+    let thread_name = format!("{}: [{}]", protocol.runner, protocol.root.to_string_lossy());
+    thread::Builder::new()
+        .name(thread_name.clone())
+        .spawn(move || {
+            Command::new(protocol.runner.cmd.as_str())
+                .current_dir(protocol.root.as_path())
+                .args(protocol.runner.args.as_slice())
+                .output()
+                .expect("Failed to execute process")
+        })
+        .expect(&format!(
+            "Failed to launch thread: `{}`",
+            thread_name.as_str()
+        ))
+}
