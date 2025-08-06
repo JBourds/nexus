d344012c1dbc699649e33db03f434015b6123c98
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-08-06 10:37:03 -0400

    cli: Creating fuse with filenames from links.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use fuse;
 use runner;
+use std::os::unix::net::SocketAddr;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,6 +15,14 @@ struct Args {
 fn main() -> Result<()> {
     let args = Args::parse();
     let sim = config::parse(args.config.into())?;
+    let fuse = fuse::NexusFs::default();
+    let root = fuse.root();
+    let files = sim
+        .links
+        .keys()
+        .map(|link_name| SocketAddr::from_pathname(root.join(link_name)))
+        .collect::<std::io::Result<Vec<_>>>()?;
+    let fuse = fuse.with_files(files);
     runner::run(sim)?;
     Ok(())
 }
