77cd43c89f0ffc50993a3499c7cb4d855c630485
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-08-06 14:59:42 -0400

    runner: Don't actually execute the commands and wait for output, just start them and return the vector with handles to the appropriate commands.

--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -1,12 +1,14 @@
 use config::ast;
-use std::process::{Child, Command, Output, Stdio};
+use std::process::{Child, Command, Stdio};
 pub mod errors;
 use errors::*;
 
-pub fn run(sim: ast::Simulation) -> Result<(), ProtocolError> {
+pub fn run(
+    sim: &ast::Simulation,
+) -> Result<Vec<(ast::NodeHandle, ast::LinkHandle, Child)>, ProtocolError> {
     let mut processes = vec![];
-    for (node_name, node) in sim.nodes {
-        for (protocol_name, protocol) in node.protocols {
+    for (node_name, node) in &sim.nodes {
+        for (protocol_name, protocol) in &node.protocols {
             let handle = Command::new(protocol.runner.cmd.as_str())
                 .current_dir(protocol.root.as_path())
                 .stdout(Stdio::piped())
@@ -15,23 +17,9 @@ pub fn run(sim: ast::Simulation) -> Result<(), ProtocolError> {
                 .args(protocol.runner.args.as_slice())
                 .spawn()
                 .expect("Failed to execute process");
-            processes.push((node_name.clone(), protocol_name, handle));
+            processes.push((node_name.clone(), protocol_name.clone(), handle));
         }
     }
 
-    for (node_name, protocol_name, mut child) in processes {
-        let pid = child.id();
-        println!("{child:#?}");
-        let rc = child.wait().map_err(|e| ProtocolError::RunnerError {
-            node_name: node_name.clone(),
-            protocol_name: protocol_name.clone(),
-            msg: format!("{e:?}"),
-        })?;
-        let child = child.wait_with_output().unwrap();
-        println!("{node_name}: {protocol_name} PID [{pid}] - RC: {rc}");
-        println!("stdout: {}", String::from_utf8_lossy(&child.stdout));
-        println!("stderr: {}", String::from_utf8_lossy(&child.stderr));
-    }
-
-    Ok(())
+    Ok(processes)
 }
