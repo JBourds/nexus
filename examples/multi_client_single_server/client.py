41c08273c764a053f60fa65150a7cb28e652b830
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-08-15 19:49:41 -0400

    examples: Multiple client/multiple server example works even with a super fast simulation timestep.

--- examples/multi_client_single_server/client.py
+++ examples/multi_client_single_server/client.py
@@ -1,3 +1,11 @@
+"""
+client.py
+
+Each client instance receives a unique number through command line arguments
+which allow it to uniquely identify itself. They all write to the same file,
+and it is expected the file will preserve message boundaries.
+"""
+
 import os
 import sys
 import time
@@ -8,7 +16,7 @@ nexus_sock = os.path.expanduser("~/nexus/ideal")
 counter = 0
 while True:
     with open(nexus_sock, "r+") as infile:
-        print(infile.read())
+        while infile.read():
+            pass
         infile.write(f"[{counter}]: Hello from client {client_num}!")
         counter += 1
-    time.sleep(1)
--- examples/multi_client_single_server/server.py
+++ examples/multi_client_single_server/server.py
@@ -1,5 +1,11 @@
+"""
+server.py
+
+The server will receive all messages at this given link and it is expected for
+them to preserve message boundaries.
+"""
+
 import os
-import time
 
 nexus_sock = os.path.expanduser("~/nexus/ideal")
 
@@ -7,8 +13,7 @@ counter = 0
 while True:
     with open(nexus_sock, "r+") as infile:
         msg = infile.read()
-        if isinstance(msg, str):
+        if msg:
             client = 1 if "client 1" in msg else 2
-            infile.write(f"[{counter}]: Hello, client {client}!")
+            infile.write(f"[{counter}]: Hello, client {client}! (Received {msg})")
         counter += 1
-    time.sleep(1)
