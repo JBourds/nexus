76141b47a598ba1c110999f13157ca6fd2c02a13
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-08-27 20:11:52 -0400

    kernel: Cleaning up match cases for sources.

--- kernel/src/sources.rs
+++ kernel/src/sources.rs
@@ -88,6 +88,102 @@ impl Source {
         Ok(())
     }
 
+    fn poll_simulated(
+        poll: &mut Poll,
+        events: &mut Events,
+        readers: &Readers,
+        writers: &Writers,
+        router: &mut Router,
+        delta: Duration,
+    ) -> Result<(), SourceError> {
+        // Check write events
+        poll.poll(events, Some(delta))
+            .map_err(|_| SourceError::PollError)?;
+        for event in events.iter() {
+            let Token(index) = event.token();
+            router
+                .receive_write(index)
+                .map_err(SourceError::RouterError)?;
+        }
+        for writer in writers.iter() {
+            while writer.request.try_recv().is_ok() {
+                let _ = writer.ack.send(WriteSignal::Done);
+            }
+        }
+        router.step().map_err(SourceError::RouterError)?;
+        for (index, reader) in readers.iter().enumerate() {
+            while reader.request.try_recv().is_ok() {
+                let _ = reader.ack.send(
+                    router
+                        .deliver_msg(index)
+                        .map_err(SourceError::RouterError)?,
+                );
+            }
+        }
+        Ok(())
+    }
+
+    fn poll_log(
+        src: &mut BufReader<File>,
+        ts: Timestep,
+        readers: &Readers,
+        router: &mut Router,
+        next_log: &mut Option<BinaryLogRecord>,
+    ) -> Result<(), SourceError> {
+        // Only do this I/O if we either don't know when the next log
+        // is or if we know there are logs ready to be sent.
+        if next_log.as_ref().is_none_or(|rec| rec.timestep <= ts) {
+            if let Some(Err(e)) = next_log
+                .take()
+                .map(|rec| router.post_to_mailboxes(rec.node, rec.channel, rec.data))
+            {
+                return Err(SourceError::RouterError(e));
+            }
+
+            loop {
+                let config = config::standard();
+                match bincode::decode_from_reader::<BinaryLogRecord, _, _>(&mut *src, config) {
+                    Ok(BinaryLogRecord {
+                        is_publisher: false,
+                        ..
+                    }) => break Err(SourceError::InvalidLogType),
+                    // Record scheduled for the future
+                    Ok(rec) if rec.timestep > ts => {
+                        *next_log = Some(rec);
+                        break Ok(());
+                    }
+                    Ok(BinaryLogRecord {
+                        node,
+                        channel,
+                        data,
+                        ..
+                    }) => {
+                        if let Err(e) = router.post_to_mailboxes(node, channel, data) {
+                            break Err(SourceError::RouterError(e));
+                        }
+                    }
+                    Err(DecodeError::Io { inner, .. })
+                        if inner.kind() == io::ErrorKind::UnexpectedEof =>
+                    {
+                        break Ok(());
+                    }
+                    Err(e) => break Err(SourceError::ReplayLogRead(e)),
+                }
+            }?;
+        }
+        router.step().map_err(SourceError::RouterError)?;
+        for (index, reader) in readers.iter().enumerate() {
+            while reader.request.try_recv().is_ok() {
+                let _ = reader.ack.send(
+                    router
+                        .deliver_msg(index)
+                        .map_err(SourceError::RouterError)?,
+                );
+            }
+        }
+        Ok(())
+    }
+
     pub(crate) fn poll(
         &mut self,
         router: &mut Router,
@@ -100,92 +196,12 @@ impl Source {
                 events,
                 readers,
                 writers,
-            } => {
-                // Check write events
-                poll.poll(events, Some(delta))
-                    .map_err(|_| SourceError::PollError)?;
-                for event in events.iter() {
-                    let Token(index) = event.token();
-                    router
-                        .receive_write(index)
-                        .map_err(SourceError::RouterError)?;
-                }
-                for writer in writers.iter() {
-                    while writer.request.try_recv().is_ok() {
-                        let _ = writer.ack.send(WriteSignal::Done);
-                    }
-                }
-                router.step().map_err(SourceError::RouterError)?;
-                for (index, reader) in readers.iter().enumerate() {
-                    while reader.request.try_recv().is_ok() {
-                        let _ = reader.ack.send(
-                            router
-                                .deliver_msg(index)
-                                .map_err(SourceError::RouterError)?,
-                        );
-                    }
-                }
-            }
+            } => Self::poll_simulated(poll, events, readers, writers, router, delta),
             Self::Replay {
                 src,
                 readers,
                 next_log,
-            } => {
-                // Only do this I/O if we either don't know when the next log
-                // is or if we know there are logs ready to be sent.
-                if next_log.as_ref().is_none_or(|rec| rec.timestep <= ts) {
-                    if let Some(Err(e)) = next_log
-                        .take()
-                        .map(|rec| router.post_to_mailboxes(rec.node, rec.channel, rec.data))
-                    {
-                        return Err(SourceError::RouterError(e));
-                    }
-
-                    loop {
-                        let config = config::standard();
-                        match bincode::decode_from_reader::<BinaryLogRecord, _, _>(
-                            &mut *src, config,
-                        ) {
-                            Ok(BinaryLogRecord {
-                                is_publisher: false,
-                                ..
-                            }) => break Err(SourceError::InvalidLogType),
-                            // Record scheduled for the future
-                            Ok(rec) if rec.timestep > ts => {
-                                *next_log = Some(rec);
-                                break Ok(());
-                            }
-                            Ok(BinaryLogRecord {
-                                node,
-                                channel,
-                                data,
-                                ..
-                            }) => {
-                                if let Err(e) = router.post_to_mailboxes(node, channel, data) {
-                                    break Err(SourceError::RouterError(e));
-                                }
-                            }
-                            Err(DecodeError::Io { inner, .. })
-                                if inner.kind() == io::ErrorKind::UnexpectedEof =>
-                            {
-                                break Ok(());
-                            }
-                            Err(e) => break Err(SourceError::ReplayLogRead(e)),
-                        }
-                    }?;
-                }
-                router.step().map_err(SourceError::RouterError)?;
-                for (index, reader) in readers.iter().enumerate() {
-                    while reader.request.try_recv().is_ok() {
-                        let _ = reader.ack.send(
-                            router
-                                .deliver_msg(index)
-                                .map_err(SourceError::RouterError)?,
-                        );
-                    }
-                }
-            }
+            } => Self::poll_log(src, ts, readers, router, next_log),
         }
-        Ok(())
     }
 }
