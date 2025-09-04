ac99e647e5be07b75c25d1fc7e6373239df91e44
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-09-03 21:24:44 -0400

    runner: Using cgroupfs to relocate the kernel process and create a new sunhierarchy for the processes it will start.

--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -1,6 +1,9 @@
 use config::ast;
 use std::{
     fmt::Display,
+    fs::{self, File, OpenOptions},
+    io::{Read, Write},
+    path::PathBuf,
     process::{Child, Command, Stdio},
     str::FromStr,
 };
@@ -50,10 +53,39 @@ impl Display for RunCmd {
     }
 }
 
+/// Move the current process out of its automatically assigned systemd cgroup
+/// into a new one within the hierarchy to appease the "no internal processes"
+/// rule. Creates subhierarchy for node protocols as well.
+fn setup_managed_cgroup() -> PathBuf {
+    let pid = std::process::id();
+    let parent_cgroup = PathBuf::from(format!("/proc/{pid}/cgroup"));
+    let mut buf = String::new();
+    let _ = File::open(parent_cgroup).unwrap().read_to_string(&mut buf);
+    let cgroup_path = PathBuf::from(format!(
+        "/sys/fs/cgroup{}",
+        buf.split(":").last().unwrap().trim_end()
+    ));
+
+    let kernel_cgroup_path = cgroup_path.join("kernel");
+    let node_cgroup_path = kernel_cgroup_path.join("nodes");
+    fs::create_dir_all(&node_cgroup_path).unwrap();
+
+    let mut kernel_cgroup_procs = OpenOptions::new()
+        .write(true)
+        .open(kernel_cgroup_path.join("cgroup.procs"))
+        .unwrap();
+    let _ = kernel_cgroup_procs
+        .write(pid.to_string().as_bytes())
+        .unwrap();
+
+    node_cgroup_path
+}
+
 /// Execute all the protocols on every node in their own process.
 /// Returns a result with a vector of handles to refer to running processes.
 pub fn run(sim: &ast::Simulation) -> Result<Vec<RunHandle>, ProtocolError> {
     let mut processes = vec![];
+    let node_cgroups = setup_managed_cgroup();
     for (node_name, node) in &sim.nodes {
         for (protocol_name, protocol) in &node.protocols {
             let process = Command::new(protocol.runner.cmd.as_str())
