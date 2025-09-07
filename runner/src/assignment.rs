e6d028795ab8dfba312ae0a44e7015d4c8253982
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2025-09-07 19:29:55 -0400

    runner: CPU set assignment algorithm.

--- runner/src/assignment.rs
+++ runner/src/assignment.rs
@@ -5,6 +5,61 @@ use sysinfo::{Cpu, CpuRefreshKind, RefreshKind, System};
 
 pub type Frequency = u64;
 pub type CpuNum = usize;
+
+#[derive(Debug, Default, Clone)]
+pub struct Cpuset(String);
+impl Cpuset {
+    fn from_cpus(cpus: &[usize]) -> Cpuset {
+        let mut s = String::new();
+        for cpu in cpus {
+            s.push_str(&cpu.to_string());
+            s.push(',');
+        }
+        if !s.is_empty() {
+            s.pop().unwrap();
+        }
+        Cpuset(s)
+    }
+}
+
+impl std::fmt::Display for Cpuset {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Debug)]
+pub struct CpuAssignment {
+    pub cpusets: HashMap<Frequency, Vec<CpuNum>>,
+    pub available: HashMap<Frequency, Frequency>,
+}
+
+impl CpuAssignment {
+    pub fn new() -> Self {
+        let cpusets = get_cpusets();
+        let available = cpusets
+            .iter()
+            .map(|(key, cpus)| (*key, *key * cpus.len() as u64))
+            .collect();
+        Self { cpusets, available }
+    }
+
+    /// Given a required number of clock cycles, assign it to a set of
+    pub fn assign(&mut self, required: Frequency) -> Cpuset {
+        if let Some((key, available)) = self
+            .available
+            .iter_mut()
+            .filter(|(_, available)| **available >= required)
+            .max_by_key(|(_, available)| **available)
+        {
+            *available -= required;
+            Cpuset::from_cpus(&self.cpusets[key])
+        } else {
+            Cpuset::default()
+        }
+    }
+}
+
 /// Try this two ways:
 ///     1, Directly with `lscpu` to query max megahertz.
 ///     2, If the previous way didn`t work, assume there is no frequency
@@ -42,4 +97,53 @@ fn get_cpusets() -> HashMap<Frequency, Vec<CpuNum>> {
         }
     }
     cpusets
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn assignments() {
+        const GHZ: u64 = 1 << 30;
+        let cpusets: HashMap<_, _> = [
+            (5 * GHZ, vec![0, 1]),
+            (4 * GHZ, vec![2, 3]),
+            (3 * GHZ, vec![4, 5]),
+        ]
+        .into_iter()
+        .collect();
+        let available: HashMap<_, _> = cpusets
+            .iter()
+            .map(|(freq, cpus)| (*freq, *freq * cpus.len() as u64))
+            .collect();
+        let mut assignments = CpuAssignment { cpusets, available };
+        let test = [
+            // Allocation goes to the greatest available
+            (1 * GHZ, "0,1".to_string()),
+            // Edge case - make sure we still get allocation
+            (1 * GHZ - 1, "0,1".to_string()),
+            (3, "0,1".to_string()),
+            // Next allocation goes to next highest cpuset
+            (1, "2,3".to_string()),
+            (2 * GHZ, "2,3".to_string()),
+            // This cpuset is back to largest, exhaust it
+            (8 * GHZ - 2, "0,1".to_string()),
+            // Slowest CPUs now have the most availability, get it to have one
+            // less than 2,3
+            (2, "4,5".to_string()),
+            // Exhaust remaining capacities
+            (6 * GHZ - 1, "2,3".to_string()),
+            (6 * GHZ - 2, "4,5".to_string()),
+            // Make sure no set is obtained
+            (1, "".to_string()),
+        ];
+        for (input, expected) in test {
+            let cpuset = assignments.assign(input);
+            assert_eq!(
+                expected, cpuset.0,
+                "Made assignment for {input}Hz and expected {expected} but got {cpuset}"
+            );
+        }
+    }
 }
