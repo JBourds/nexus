95c9ba20f5b3903053903e21cb395f97899a4367
Jordan Bourdeau <jordanbourdeau03@gmail.com>
2026-02-22 16:01:32 -0500

    example: Added test that the value matches what was configured at the start.

--- examples/time/src/main.cpp
+++ examples/time/src/main.cpp
@@ -12,10 +12,15 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <cassert>
+#include <cstdlib>
+
 #define NFILES 3
 #define SECONDS 0
 #define MILLIS 1
 #define MICROS 2
+
+#define EXPECTED_START_S 1767268800
 
 #define JAN_1_2026_S 1767225600
 #define JAN_1_2026_MS 1767225600000
@@ -30,11 +35,13 @@ const char* PATHS[NFILES] = {
 
 void open_files();
 void read_files();
+uint64_t read_file(int fd);
 void write_time(size_t index, uint64_t val);
 
 int main() {
     setbuf(stdout, NULL);
     open_files();
+    assert(read_file(FDS[SECONDS]) == EXPECTED_START_S);
     read_files();
 
     write_time(SECONDS, JAN_1_2026_S);
@@ -62,32 +69,38 @@ void open_files() {
     }
 }
 
-void read_files() {
+uint64_t read_file(int fd) {
     char buf[64];
-    tm* time;
+    ssize_t nread = read(fd, buf, sizeof(buf));
+    if (nread < 0) {
+        fprintf(stderr, "Error reading time file.");
+        exit(EXIT_FAILURE);
+    }
+    buf[nread] = '\0';
+    char* nptr = NULL;
+    uint64_t val = strtoull(buf, &nptr, 10);
+    return val;
+}
+
+void read_files() {
     for (size_t i = 0; i < NFILES; ++i) {
-        ssize_t nread = read(FDS[i], buf, sizeof(buf));
-        if (nread < 0) {
-            fprintf(stderr, "Error reading time file.");
-            exit(EXIT_FAILURE);
-        }
-        buf[nread] = '\0';
-        char* nptr = NULL;
-        uint64_t ms_since_epoch = strtoull(buf, &nptr, 10);
-        printf("%s Epoch: %llu\n", PATHS[i],
-               (unsigned long long)ms_since_epoch);
+        uint64_t val = read_file(FDS[i]);
+        printf("%s Epoch: %llu\n", PATHS[i], (unsigned long long)val);
     }
 }
 void write_time(size_t index, uint64_t val) {
     char buf[64];
-    size_t n = snprintf(buf, sizeof(buf), "%llu", (unsigned long long)val);
-    if (sizeof(buf) < n) {
+    int n = snprintf(buf, sizeof(buf), "%llu", (unsigned long long)val);
+    assert(n > 0);
+    size_t bytes_required = (size_t)n;
+    if (sizeof(buf) < bytes_required) {
         fprintf(stderr, "Buffer was too small to fit %llu bytes",
                 (unsigned long long)n);
         exit(EXIT_FAILURE);
     }
-    ssize_t rc = write(FDS[index], buf, n);
-    if (rc != n) {
+    ssize_t rc = write(FDS[index], buf, bytes_required);
+    assert(rc > 0);
+    if ((size_t)rc != bytes_required) {
         fprintf(
             stderr,
             "Didn't write correct number of bytes (got %lld expected %lld)\n",
